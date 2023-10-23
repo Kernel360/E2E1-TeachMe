5b26456cf9ecf6a9638b633e8c30a26e91c0a044
StoneHee99 <kiit0901@gmail.com>
2023-10-23 16:33:53 +0900

    Project files upload

---
+++ src/test/java/kr/kernel360/teachme/TeachmeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.kernel360.teachme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
