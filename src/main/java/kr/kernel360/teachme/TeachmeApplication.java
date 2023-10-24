88ae8c4c1dd1c195476edb0a41b530afc425c435
StoneHee99 <kiit0901@gmail.com>
2023-10-24 18:31:29 +0900

    feat: Update Entity Test logic

--- src/main/java/kr/kernel360/teachme/TeachmeApplication.java
+++ src/main/java/kr/kernel360/teachme/TeachmeApplication.java
@@ -1,9 +1,13 @@
 package kr.kernel360.teachme;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+
 public class TeachmeApplication {
 
     public static void main(String[] args) {
