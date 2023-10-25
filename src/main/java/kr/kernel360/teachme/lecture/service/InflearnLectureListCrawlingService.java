056ceecbc6608691a269b0df50a7302fcfef6d00
StoneHee99 <kiit0901@gmail.com>
2023-10-25 14:15:58 +0900

    refactor: refactoring InflearnLectureListCrawlingService

--- src/main/java/kr/kernel360/teachme/lecture/service/InflearnLectureListCrawlingService.java
+++ src/main/java/kr/kernel360/teachme/lecture/service/InflearnLectureListCrawlingService.java
@@ -19,13 +19,13 @@ public class InflearnLectureListCrawlingService {
 	private final static String TARGET_URL = "https://www.inflearn.com/courses";
 	private final static String PAGE_URL = "https://www.inflearn.com/courses?order=seq&page=";
 
-	public static void main(String[] args) throws IOException {
 
+	private static List<InflearnLectureListResponse> crawlInflearnLectureList() throws IOException {
 		Connection conn = Jsoup.connect(TARGET_URL);
 		Document doc = conn.get();
 		int pageNum = getPageNumFromInflearn(doc);
 
-		if(pageNum == 0) return;
+		if(pageNum == 0) return null;
 
 		List<InflearnLectureListResponse> crawledDataList = new ArrayList<>();
 
@@ -35,35 +35,13 @@ public class InflearnLectureListCrawlingService {
 			Document document = pageConn.get();
 			Elements courseElements = document.select("div.course");
 
-            for (Element course : courseElements) {
-				InflearnLectureListResponse inflearnCourse = new InflearnLectureListResponse();
-				inflearnCourse.setTitle(course.select("div.course_title").text());
-				inflearnCourse.setImageSource(course.select("figure.is_thumbnail > img").attr("src"));
-				inflearnCourse.setStudentCnt(course.select("span.student_cnt").text());
-				inflearnCourse.setInstructor(course.select("div.instructor").text());
-
-				if(!course.select("div.price > del").isEmpty()) {
-					inflearnCourse.setRealIntPrice(Integer.parseInt(StringUtil.removeNotNumeric(course.select("div.price > del")
-							.text())));
-					inflearnCourse.setSaleIntPrice(Integer.parseInt(StringUtil.removeNotNumeric(course.select("div.price > span.pay_price")
-							.text())));
-				} else {
-					int price = StringUtil.isNumeric(StringUtil.removeNotNumeric(course.select("div.price").text()))
-							? Integer.parseInt(StringUtil.removeNotNumeric(course.select("div.price").text())) : 0;
-					inflearnCourse.setRealIntPrice(price);
-					inflearnCourse.setSaleIntPrice(price);
-				}
-
-				inflearnCourse.setUrl(MASTER_URL + course.select("a.e_course_click").attr("href"));
-				inflearnCourse.setDescription(course.select("p.course_description").text());
-				inflearnCourse.setDifficulty(course.select("div.course_level > span").text());
-				inflearnCourse.setSkills(course.select("div.course_skills > span").text());
-
+			for (Element course : courseElements) {
+				InflearnLectureListResponse inflearnCourse = buildCourseToObject(course);
 				crawledDataList.add(inflearnCourse);
-            }
+			}
 			if(crawledDataList.size() > 10) break;
 		}
-		System.out.println(crawledDataList.get(1).getSkills());
+		return crawledDataList;
 	}
 
 	private static int getPageNumFromInflearn(Document doc) {
@@ -78,5 +56,37 @@ public class InflearnLectureListCrawlingService {
 			}
 		}
 		return 0;
+	}
+
+	private static InflearnLectureListResponse buildCourseToObject(Element course) {
+		InflearnLectureListResponse inflearnCourse = new InflearnLectureListResponse();
+		inflearnCourse.setTitle(course.select("div.course_title").text());
+		inflearnCourse.setImageSource(course.select("figure.is_thumbnail > img").attr("src"));
+		inflearnCourse.setStudentCnt(course.select("span.student_cnt").text());
+		inflearnCourse.setInstructor(course.select("div.instructor").text());
+
+		if(!course.select("div.price > del").isEmpty()) {
+			inflearnCourse.setRealIntPrice(Integer.parseInt(StringUtil.removeNotNumeric(course.select("div.price > del")
+					.text())));
+			inflearnCourse.setSaleIntPrice(Integer.parseInt(StringUtil.removeNotNumeric(course.select("div.price > span.pay_price")
+					.text())));
+		} else {
+			int price = StringUtil.isNumeric(StringUtil.removeNotNumeric(course.select("div.price").text()))
+					? Integer.parseInt(StringUtil.removeNotNumeric(course.select("div.price").text())) : 0;
+			inflearnCourse.setRealIntPrice(price);
+			inflearnCourse.setSaleIntPrice(price);
+		}
+
+		inflearnCourse.setUrl(MASTER_URL + course.select("a.e_course_click").attr("href"));
+		inflearnCourse.setDescription(course.select("p.course_description").text());
+		inflearnCourse.setDifficulty(course.select("div.course_level > span").text());
+		inflearnCourse.setSkills(course.select("div.course_skills > span").text());
+
+		return inflearnCourse;
+	}
+
+	public static void main(String[] args) throws IOException {
+		List<InflearnLectureListResponse> crawledDataList = crawlInflearnLectureList();
+		System.out.println(crawledDataList.get(1).getSkills());
 	}
 }
