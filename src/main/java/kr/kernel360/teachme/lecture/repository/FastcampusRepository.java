942a2e68f6c112ae138d2a2de20cb3e5c9fedba6
Yejin Song <yjyjsssong@gmail.com>
2023-10-25 17:02:26 +0900

    feat: add fastcampus entity,repository

---
+++ src/main/java/kr/kernel360/teachme/lecture/repository/FastcampusRepository.java
@@ -0,0 +1,7 @@
+package kr.kernel360.teachme.lecture.repository;
+
+import kr.kernel360.teachme.lecture.entity.Fastcampus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FastcampusRepository extends JpaRepository<Fastcampus,Long> {
+}
