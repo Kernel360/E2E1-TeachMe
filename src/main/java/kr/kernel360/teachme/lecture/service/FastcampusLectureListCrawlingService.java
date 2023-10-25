4b509e9254aacc2b45f44b4baa8133c989e1fb07
StoneHee99 <kiit0901@gmail.com>
2023-10-25 11:42:25 +0900

    refactor: refactoring FastcampusLectureListCrawlingService

--- src/main/java/kr/kernel360/teachme/lecture/service/FastcampusLectureListCrawlingService.java
+++ src/main/java/kr/kernel360/teachme/lecture/service/FastcampusLectureListCrawlingService.java
@@ -1,28 +1,25 @@
 package kr.kernel360.teachme.lecture.service;
 
 import kr.kernel360.teachme.lecture.dto.FastcampusResponse;
+import kr.kernel360.teachme.lecture.dto.FastcampustLectureListResponse;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 public class FastcampusLectureListCrawlingService {
-    private String apiUrl;
 
-    public FastcampusLectureListCrawlingService(String apiUrl) {
-        this.apiUrl = apiUrl;
-    }
 
-    public FastcampusResponse getFastcampusResponse() {
+    public static FastcampustLectureListResponse getFastcampusResponse() {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        FastcampusResponse fastcampusResponse = restTemplate.getForObject("https://fastcampus.co.kr/.api/www/categories/1/page", FastcampusResponse.class);
-
-
+        FastcampustLectureListResponse fastcampusResponse = restTemplate.getForObject("https://fastcampus.co.kr/.api/www/categories/all", FastcampustLectureListResponse.class);
 
         return fastcampusResponse;
     }
 
     public static void main(String[] args) {
-        FastcampusLectureListCrawlingService crawl = new FastcampusLectureListCrawlingService("https://fastcampus.co.kr/.api/www/categories/1/page");
-        System.out.println(crawl.getFastcampusResponse().getData().getCategoryInfo().getCourses().get(0).toString());
+        FastcampustLectureListResponse crawledData = getFastcampusResponse();
+        System.out.println(crawledData.getData().getCategoryList().get(1).getCourses().get(0).getDesktopCardAsset());
     }
 }
