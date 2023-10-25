c10962b1c179b3db6a0002b927d14478bb56c7d2
StoneHee99 <kiit0901@gmail.com>
2023-10-25 12:39:04 +0900

    feat: add Fastcampus crawler for crawl all data at once

---
+++ src/main/java/kr/kernel360/teachme/lecture/dto/FastcampustLectureResponse.java
@@ -0,0 +1,16 @@
+package kr.kernel360.teachme.lecture.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class FastcampustLectureResponse {
+    private Long id;
+    private String state;
+    private String slug;
+    private String publicTitle;
+    private String publicDescription;
+    private String keywords;
+    private String desktopCardAsset;
+}
--- src/main/java/kr/kernel360/teachme/lecture/service/FastcampusLectureListCrawlingService.java
+++ src/main/java/kr/kernel360/teachme/lecture/service/FastcampusLectureListCrawlingService.java
@@ -2,12 +2,15 @@ package kr.kernel360.teachme.lecture.service;
 
 import kr.kernel360.teachme.lecture.dto.FastcampusResponse;
 import kr.kernel360.teachme.lecture.dto.FastcampustLectureListResponse;
+import kr.kernel360.teachme.lecture.dto.FastcampustLectureResponse;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FastcampusLectureListCrawlingService {
-
 
     public static FastcampustLectureListResponse getFastcampusResponse() {
 
@@ -17,9 +20,20 @@ public class FastcampusLectureListCrawlingService {
 
         return fastcampusResponse;
     }
+    public static List<FastcampustLectureResponse> convertLectureListToLecture(FastcampustLectureListResponse lecutreList) {
+        List<FastcampustLectureResponse> lectures = new ArrayList<>();
+        ModelMapper modelMapper = new ModelMapper();
+        for(var i = 0; i < lecutreList.getData().getCategoryList().size(); i++) {
+            for(var j = 0; j < lecutreList.getData().getCategoryList().get(i).getCourses().size(); j++) {
+                lectures.add(modelMapper.map(lecutreList.getData().getCategoryList().get(i).getCourses().get(j), FastcampustLectureResponse.class));
+            }
+        }
+        return lectures;
+    }
 
     public static void main(String[] args) {
         FastcampustLectureListResponse crawledData = getFastcampusResponse();
-        System.out.println(crawledData.getData().getCategoryList().get(1).getCourses().get(0).getDesktopCardAsset());
+        List<FastcampustLectureResponse> lectures = convertLectureListToLecture(crawledData);
+        System.out.println(lectures.get(0).toString());
     }
 }
