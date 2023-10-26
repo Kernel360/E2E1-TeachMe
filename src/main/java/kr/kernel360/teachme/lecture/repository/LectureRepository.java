282df39af3edb6df95c03f5d42f4c3559b4d29a5
StoneHee99 <kiit0901@gmail.com>
2023-10-27 01:41:35 +0900

    feat: add load latest updated lecture

--- src/main/java/kr/kernel360/teachme/lecture/repository/LectureRepository.java
+++ src/main/java/kr/kernel360/teachme/lecture/repository/LectureRepository.java
@@ -1,9 +1,14 @@
 package kr.kernel360.teachme.lecture.repository;
 
 import kr.kernel360.teachme.lecture.entity.Lecture;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
+
+    List<Lecture> findByOrderByIdDesc(Pageable pageable);
 }
