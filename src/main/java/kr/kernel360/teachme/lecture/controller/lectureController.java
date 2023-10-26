66142a7737f4bb1e5c9e8191841f9cce347f041b
Yejin Song <yjyjsssong@gmail.com>
2023-10-26 15:02:39 +0900

    fix: fastcampuscrawlingservice

--- src/main/java/kr/kernel360/teachme/lecture/controller/lectureController.java
+++ src/main/java/kr/kernel360/teachme/lecture/controller/lectureController.java
@@ -1,14 +1,30 @@
 package kr.kernel360.teachme.lecture.controller;
 
+import kr.kernel360.teachme.lecture.dto.FastcampustLectureResponse;
+import kr.kernel360.teachme.lecture.entity.FastcampusLecture;
+import kr.kernel360.teachme.lecture.service.FastcampusLectureListCrawlingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/lecture")
 public class lectureController {
+//    @GetMapping("/view")
+//    public String viewLecture() {
+//        return "hi";
+//    }
+
+    private final FastcampusLectureListCrawlingService fastcampusLectureListCrawlingService;
     @GetMapping("/view")
-    public String viewLecture() {
-        return "hi";
+    public List<FastcampustLectureResponse> viewLecture() {
+        return fastcampusLectureListCrawlingService.create();
     }
+
+
+
 }
