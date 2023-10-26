4bcb72d904a80f2866c266b6247106f5cf966993
StoneHee99 <kiit0901@gmail.com>
2023-10-26 15:18:30 +0900

    Feat: update feature inflearn lecture crawler

--- src/main/java/kr/kernel360/teachme/lecture/controller/CrawlerController.java
+++ src/main/java/kr/kernel360/teachme/lecture/controller/CrawlerController.java
@@ -1,8 +1,12 @@
 package kr.kernel360.teachme.lecture.controller;
 
+import io.swagger.annotations.ApiOperation;
+import kr.kernel360.teachme.exception.CrawlerException;
 import kr.kernel360.teachme.lecture.dto.CrawlingRequest;
 import kr.kernel360.teachme.lecture.service.InflearnLectureListCrawlingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -13,19 +17,27 @@ public class CrawlerController {
 
     private final InflearnLectureListCrawlingService inflearnLectureListCrawlingService;
 
+    @ApiOperation(value="어드민 크롤러 사이트", notes="crawlerForm.html return")
     @GetMapping("")
     public ModelAndView getCrawlerPage() {
         return new ModelAndView("crawler/crawlerForm");
     }
 
+    @ApiOperation(value="어드민 크롤링 요청", notes="요청 Parameter에 따라 크롤러 작동")
     @PostMapping("/crawling")
-    public String crawlLectureData(@RequestBody CrawlingRequest crawling) {
+    public ResponseEntity<String> crawlLectureData(@RequestBody CrawlingRequest crawling) {
         if(crawling.getPlatform().equals("fastcampus")) {
 
         } else {
-            inflearnLectureListCrawlingService.runInflearnLectureCrawler();
+            try {
+                inflearnLectureListCrawlingService.runInflearnLectureCrawler();
+                return ResponseEntity.ok("크롤링 성공");
+            } catch (CrawlerException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("크롤링 실패: " + e.getMessage());
+            }
+
         }
-        return "success";
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("크롤링 실패: 요청 양식이 맞지 않습니다.");
     }
 
 }
--- src/main/java/kr/kernel360/teachme/lecture/controller/lectureController.java
+++ src/main/java/kr/kernel360/teachme/lecture/controller/lectureController.java
@@ -1,33 +1,17 @@
 package kr.kernel360.teachme.lecture.controller;
 
-import kr.kernel360.teachme.lecture.dto.FastcampustLectureResponse;
-import kr.kernel360.teachme.lecture.entity.FastcampusLecture;
-import kr.kernel360.teachme.lecture.service.FastcampusLectureListCrawlingService;
-import lombok.RequiredArgsConstructor;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
-@RequiredArgsConstructor
 @RequestMapping("/lecture")
 public class lectureController {
 
-    private final FastcampusLectureListCrawlingService fastcampusLectureListCrawlingService;
-    @GetMapping("/view")
-    public List<FastcampustLectureResponse> viewLecture() {
-        return fastcampusLectureListCrawlingService.create();
-
-
-    @ApiOperation(value="강의 크롤링을 위한 사이트", notes="정상 동작 시 페이지 return")
+    @ApiOperation(value="테스트 사이트", notes="정상 동작 시 페이지 return")
     @GetMapping("/admin")
     public String viewTest() {
         return "hi";
     }
-
-
-
 }
