c0ae6abe322a21d644ee33f1720e3883aa4ff887
StoneHee99 <kiit0901@gmail.com>
2023-10-30 16:20:23 +0900

    feat: update inflearn detail crawler

--- src/main/java/kr/kernel/teachme/lecture/controller/LectureController.java
+++ src/main/java/kr/kernel/teachme/lecture/controller/LectureController.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
