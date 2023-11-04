1aafb1015dfb0fb8fc6defbc10170c8d4759741c
StoneHee99 <kiit0901@gmail.com>
2023-11-04 11:39:17 +0900

    style: add a style at lecture detail page

--- src/main/java/kr/kernel/teachme/common/config/InitializeDefaultConfig.java
+++ src/main/java/kr/kernel/teachme/common/config/InitializeDefaultConfig.java
@@ -1,7 +1,7 @@
 package kr.kernel.teachme.common.config;
 
-import lombok.RequiredArgsConstructor;
 import kr.kernel.teachme.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -23,7 +23,7 @@ public class InitializeDefaultConfig {
      */
     @Bean
     public void initializeDefaultUser() {
-//        Member member = memberService.signup("user", "user");
+       //memberService.signup("user", "user");
 //        noteService.saveNote(user, "테스트", "테스트입니다.");
 //        noteService.saveNote(user, "테스트2", "테스트2입니다.");
 //        noteService.saveNote(user, "테스트3", "테스트3입니다.");
@@ -35,7 +35,7 @@ public class InitializeDefaultConfig {
      */
     @Bean
     public void initializeDefaultAdmin() {
-//        memberService.signupAdmin("admin", "admin");
+       //memberService.signupAdmin("admin", "admin");
 //        noticeService.saveNotice("환영합니다.", "환영합니다 여러분");
 //        noticeService.saveNotice("노트 작성 방법 공지", "1. 회원가입\n2. 로그인\n3. 노트 작성\n4. 저장\n* 본인 외에는 게시글을 볼 수 없습니다.");
     }
