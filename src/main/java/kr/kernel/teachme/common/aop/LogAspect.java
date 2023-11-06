f9b7d8053bfd99513cc6d060b553cfc1cbece293
Yejin Song <yjyjsssong@gmail.com>
2023-11-06 12:41:20 +0900

    faet : log with spring aop

---
+++ src/main/java/kr/kernel/teachme/common/aop/LogAspect.java
@@ -0,0 +1,46 @@
+package kr.kernel.teachme.common.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@Aspect
+@Slf4j
+public class LogAspect {
+
+    @Pointcut("within(kr.kernel.teachme.domain.crawler.controller.*) || " +
+            "within(kr.kernel.teachme.domain.lecture.controller.*) || " +
+            "within(kr.kernel.teachme.domain.member.controller.*)")
+    public void controller(){
+
+    }
+
+    @Before("controller()")
+    public void beforeRequest(JoinPoint joinPoint) {
+        log.trace("###Start request {}", joinPoint.getSignature().toShortString());
+        Arrays.stream(joinPoint.getArgs())
+                .map(Object::toString)
+                .map(str -> "\t" + str)
+                .forEach(log::info);
+    }
+
+    @AfterReturning(pointcut = "controller()", returning = "returnValue")
+    public void afterReturningLogging(JoinPoint joinPoint, Object returnValue) {
+        log.info("###End request {}", joinPoint.getSignature().toShortString());
+
+        if (returnValue == null) return;
+
+        log.info("\t{}", returnValue.toString());
+    }
+
+    @AfterThrowing(pointcut = "controller()", throwing = "e")
+            public void afterThrowingLogging(JoinPoint joinPoint, Exception e) {
+        log.error("###Occured error in request {}", joinPoint.getSignature().toShortString());
+        log.error("\t{}", e.getMessage());
+    }
+
+}
