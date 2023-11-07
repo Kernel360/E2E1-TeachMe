231d006e7f7257019681a5e936290b61daf15148
StoneHee99 <kiit0901@gmail.com>
2023-11-07 14:04:06 +0900

    style: add style with detail page

---
+++ src/test/java/kr/kernel/teachme/common/config/PasswordEncoderConfigTest.java
@@ -0,0 +1,22 @@
+package kr.kernel.teachme.common.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PasswordEncoderConfigTest {
+
+    private final PasswordEncoderConfig passwordEncoderConfig = new PasswordEncoderConfig();
+
+    @Test
+    void passwordEncoderBeanShouldBeCreated() {
+        PasswordEncoder passwordEncoder = passwordEncoderConfig.passwordEncoder();
+        assertThat(passwordEncoder).isNotNull();
+        assertThat(passwordEncoder).isInstanceOf(PasswordEncoder.class);
+
+        String rawPassword = "password";
+        String encodedPassword = passwordEncoder.encode(rawPassword);
+        assertThat(passwordEncoder.matches(rawPassword, encodedPassword));
+    }
+}
