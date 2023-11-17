ac65b4d778d5956ae2921e9db70dcfd6d4092eec
StoneHee99 <kiit0901@gmail.com>
2023-11-17 14:18:14 +0900

    feat: add feature to edit my review

--- src/main/resources/static/js/member/review.js
+++ src/main/resources/static/js/member/review.js
@@ -1,3 +1,5 @@
+let currentReviewId;
+
 function showEditForm(reviewId) {
     const reviewBox = document.createElement("div");
     reviewBox.className = "rating_box";
@@ -13,7 +15,7 @@ function showEditForm(reviewId) {
     reviewLabel.style.display = "flex";
 
     reviewLabel.innerHTML = "<textarea id=\"review_text\" placeholder=\"당신의 리뷰를 남겨주세요 ✏️\" class=\"review_area\"></textarea>\n" +
-        "                    <button id=\"review_button\" class=\"review_button\">\n" +
+        "                    <button id=\"review_button\" class=\"review_button\" onclick=\"edit(" + reviewId + ")\">\n" +
         "                        <i class=\"fa-solid fa-pen-to-square\"></i>\n" +
         "                    </button>"
 
@@ -29,6 +31,49 @@ function showEditForm(reviewId) {
     rating_input.addEventListener('change', () => {
         rating_star.style.width = `${rating_input.value * 10}%`;
     });
+
+    currentReviewId = reviewId;
+}
+
+function edit() {
+    if(currentReviewId === null) {
+        alert("리뷰 수정 중 오류가 발생했습니다");
+        return;
+    }
+
+    const review_text = document.getElementById("review_text");
+    const rating_input = document.querySelector('.rating input');
+
+    if(review_text.value === "") {
+        alert("리뷰 내용을 작성해주세요");
+        return;
+    }
+
+    if(rating_input.value == 0) {
+        alert("리뷰 별점을 작성해주세요");
+        return;
+    }
+
+    const editData = {
+        "content" : review_text.value,
+        "score" : rating_input.value / 2,
+        "reviewId" : currentReviewId
+    };
+
+    $.ajax({
+        url:"/review/update",
+        type: "post",
+        data: JSON.stringify(editData),
+        dataType: "json",
+        contentType: "application/json;charset=UTF-8",
+        success: function() {
+            location.reload(true);
+        },
+        error: function(e) {
+            alert("리뷰 수정 중 오류가 발생하였습니다");
+            console.log(e);
+        }
+    })
 }
 
 function remove(reviewId) {
