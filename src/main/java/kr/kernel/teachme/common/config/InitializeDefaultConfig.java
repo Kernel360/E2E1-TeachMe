c14ef558af29db288045fc7aa2c17b22d07ef2b4
Yejin Song <yjyjsssong@gmail.com>
2023-11-18 14:27:22 +0900

    delete log

--- src/main/java/kr/kernel/teachme/common/config/InitializeDefaultConfig.java
+++ src/main/java/kr/kernel/teachme/common/config/InitializeDefaultConfig.java
@@ -36,7 +36,7 @@ public class InitializeDefaultConfig {
      */
     @Bean
     public void initializeDefaultAdmin() {
-       //memberService.signupAdmin("admin", "admin","관리자");
+      // memberService.signupAdmin("admin", "admin","관리자");
 //        noticeService.saveNotice("환영합니다.", "환영합니다 여러분");
 //        noticeService.saveNotice("노트 작성 방법 공지", "1. 회원가입\n2. 로그인\n3. 노트 작성\n4. 저장\n* 본인 외에는 게시글을 볼 수 없습니다.");
     }
